72e4730317411f7c07768d6426f7e966299d2a62
unknown <deedmitrij@gmail.com>
2018-11-10 21:13:07 +0200

    Made migrations

--- test_project/views.py
+++ test_project/views.py
@@ -1,17 +1,17 @@
-# from django.shortcuts import render
-# from .models import Measurement, Participant
-#
-#
-# # Create your views here.
-#
-#
-# def measurement(request):
-#     user = Participant.objects.name()
-#     # measure_list = Measurement.objects.filter()
-#     return render(request, 'user_weight.html', user)
+from django.shortcuts import render
+from .models import Measurement, Participant
 
-# from django.http import HttpResponse
-#
-#
-# def index(request):
-#     return HttpResponse("Hello, world!. You're at the Django test project.")
+
+# Create your views here.
+
+
+def measurement(request):
+    # user = Participant.objects.name()
+    # measure_list = Measurement.objects.filter()
+    return render(request, 'user_weight.html')
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world!. You're at the Django test project.")
