84dc4c5ba8e5c9484b0b2a9568b9cccb82e9473c
unknown <deedmitrij@gmail.com>
2019-04-14 01:18:41 +0300

    Added 'Forgot password' form. Changed user profile form

--- myproject/settings.py
+++ myproject/settings.py
@@ -126,9 +126,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    )
+    # 'DEFAULT_RENDERER_CLASSES': (
+    #     'rest_framework.renderers.JSONRenderer',
+    # )
 }
 
 
